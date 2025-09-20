3bba4cac697f33de51a10754a310334d9426644c
AF-Nexus <afnexus01@gmail.com>
2025-09-20 09:49:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { EventEmitter } from 'events';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __path = process.cwd();
+
+// Import your pair module (make sure pair.js is also converted to ES modules)
+import code from './pair.js';
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
 
-require('events').EventEmitter.defaultMaxListeners = 500;
+// Increase max listeners
+EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
-});
-
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/code', code);
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html');
+});
 
 app.listen(PORT, () => {
     console.log(`
@@ -24,7 +39,7 @@ Don't Forget To Give Star ‼️
 
 ᴘᴏᴡᴇʀᴅ ʙʏ ʀᴏᴏᴛ_x
 
-Server running on http://localhost:` + PORT)
+Server running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+export default app;
